3ceb9e4a6fa40e5495ea0882f70cc46cdf49399d
nazar-fomin <nazar2005kiril@gmail.com>
2025-05-26 13:08:00 +0300

    add cods

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,89 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
+async function getTodos() {
+  const request = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await request.json();
+}
+async function getPosts() {
+  const request = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return await request.json();
+}
+async function getUsers() {
+  const request = await fetch("https://jsonplaceholder.typicode.com/users");
+  return await request.json();
+}
+async function getComments() {
+  const request = await fetch("https://jsonplaceholder.typicode.com/comments");
+  return await request.json();
+}
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => setTodos(json));
-
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setPosts(json));
-  }, []);
-  console.log(todos);
+  const queryT = useQuery({ queryKey: ["todos"], queryFn: getTodos });
+  const queryP = useQuery({ queryKey: ["posts"], queryFn: getPosts });
+  const queryU = useQuery({ queryKey: ["users"], queryFn: getUsers });
+  const queryC = useQuery({ queryKey: ["comments"], queryFn: getComments });
+  const [query, setQuery] = useState("");
+  const [queryUsers, setQueryUsers] = useState("");
+  const [queryComments, setQueryComments] = useState("");
+  const filterComments = useMemo(
+    () =>
+      queryC.data?.filter((comments) =>
+        comments.name.toLowerCase().includes(queryComments.toLowerCase())
+      ),
+    [queryC.data, queryComments]
+  );
+  const filterTodos = useMemo(
+    () => queryT.data?.filter((item) => item.title.includes(query)),
+    [queryT.data, query]
+  );
+  const filterUsers = useMemo(
+    () =>
+      queryU.data?.filter((user) =>
+        user.name.toLowerCase().includes(queryUsers.toLowerCase())
+      ),
+    [queryU.data, queryUsers]
+  );
+  console.log(filterComments);
   return (
     <>
+      <form action="">
+        <label htmlFor="">Comments</label>
+        <input
+          type="text"
+          value={queryComments}
+          onChange={(event) => {
+            setQueryComments(event.target.value);
+          }}
+        />
+        <button>search</button>
+      </form>
+      <form action="">
+        <label htmlFor="">Todos</label>
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
+        />
+        <button>search</button>
+      </form>
+      <form action="">
+        <label htmlFor="">Usres</label>
+        <input
+          type="text"
+          value={queryUsers}
+          onChange={(event) => {
+            setQueryUsers(event.target.value);
+          }}
+        />
+        <button>search</button>
+      </form>
       <div className="flex flex-wrap gap-[17.5px] flex-row md:flex-col">
         <button className="flex-1 py-2 bg-blue-500 text-white rounded hidden md:block">
           4
@@ -38,12 +106,48 @@ function App() {
       <table>
         <thead>
           <tr>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterComments?.map((item) => {
+            return (
+              <tr className="users" key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterUsers?.map((item) => {
+            return (
+              <tr className="comments" key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
             <th>todos</th>
             <th>todos complet</th>
           </tr>
         </thead>
         <tbody>
-          {todos.map((item) => {
+          {filterTodos?.map((item) => {
             return (
               <tr className="todos" key={item.id}>
                 <td>{item.title}</td>
@@ -61,7 +165,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {posts.map((item) => {
+          {queryP.data?.map((item) => {
             return (
               <tr className="posts" key={item.id}>
                 <td>{item.title}</td>
